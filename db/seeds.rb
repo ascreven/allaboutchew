c194bf6f941e4ee0fef38f673fc0cf14d2a6abe9
Alexandra <akscreven@gmail.com>
2015-12-23 13:25:15 -0500

    schema is attached

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Dog.destroy_all
+
+Dog.create([{name: 'Chloe', age: 15}, {name: 'Ella', age: 5}])
