62e6ba0920ac301a3595be919ddcd76171bd8d90
Alexandra <akscreven@gmail.com>
2015-12-24 09:37:46 -0500

    dog resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-get 'dogs' => 'dogs#index'
-get 'dogs/new' => 'dogs#new'
-post 'dogs' => 'dogs#create'
-get 'dogs/:id' => 'dogs#show'
-get 'dogs/:id/edit' => 'dogs#edit'
-put 'dogs/:id' => 'dogs#update'
-delete 'dogs/:id' => 'dogs#destroy'
+root :to => 'dogs#index'
+resources :dogs do
+end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
