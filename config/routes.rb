da85a5c46b54626b92e64f44d6db34883d06f0a3
Alexandra <akscreven@gmail.com>
2016-01-02 14:17:52 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -58,3 +58,13 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  Rails.application.routes.draw do
+    devise_for :users
+    # The priority is based upon order of creation: first created -> highest priority.
+    # See how all your routes lay out with "rake routes".
+  root :to => 'dogs#index'
+  resources :dogs do
+  end
+  get '/test_ajax', to: 'dogs#test_ajax'
+  end
